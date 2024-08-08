2d2a5d98113acd19e1cc749dbc3a0c65cbb380b6
Hanna Nesterova <nesterova.hanna@gmail.com>
2024-08-08 21:57:33 +0200

    backend directories

---
+++ backend/features/resolutions/routes/ResolutionRoutes.js
@@ -0,0 +1,15 @@
+const { Router } = require("express");
+const {
+  getResolution,
+  saveResolution,
+  deleteResolution,
+  editResolution,
+} = require("../controller/ResolutionController");
+const router = Router();
+
+router.get("/", getResolution);
+router.post("/saveResolution", saveResolution);
+router.post("/deleteResolution", deleteResolution);
+router.post("/editResolution", editResolution);
+
+module.exports = router;
